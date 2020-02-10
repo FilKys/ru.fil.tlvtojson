3f535be11251b83c22caaf47a3594216fa467e2f
milli <million.fil@mail.ru>
2020-02-10 23:48:50 +0500

    Расшифровка даты, правильная. Расшифровка номера заказа Начата расшифровка имени заказчика

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,7 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.lang.Object;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Scanner;
 import java.util.logging.FileHandler;
 
@@ -20,16 +18,17 @@ public class Main {
         StringBuilder tlvString = new StringBuilder("");
         while (scanner.hasNext()) {
             tlvString.append(scanner.nextLine());
-//            byte t =  scanner.nextByte(16);
             if (scanner.hasNext())
                 tlvString.append(" ");
         }
         fileReader.close();
         System.out.println(tlvString);
         massString = tlvString.toString().split(" ");
-        String dateTime, orderNumber, customerName, items, name, price, quantity, sum;
+        String dateTimeString, orderNumberString, customerName, items, name, price, quantity, sum;
         StringBuilder s = new StringBuilder();
-        int tag = 0, length = 0, tag2, length1, length2;
+        Date dateTimeDate;
+        int tag = 0, length = 0, tag2, length1, length2, orderNumberInt;
+        Long time;
         for (int i = 0; i < massString.length; ) {
             tag2 = i + 1;
             length1 = i + 2;
@@ -40,18 +39,18 @@ public class Main {
             i += 4;
             switch (tag) {
                 case 1:
-                    dateTime = getData(i, length);
-                    System.out.println("date - " + dateTime);
-                    dateTime = dateTime.replaceAll(" ","");
-                    System.out.println("date - " + dateTime);
-                    System.out.println("date - " + Integer.parseUnsignedInt(dateTime,16));
+                    dateTimeString = getData(i, length).replaceAll(" ", "");
+//                    System.out.println("date - " + dateTime);
+                    dateTimeDate = new Date(Long.parseUnsignedLong(dateTimeString, 16) * 1000);
+                    System.out.println("date - " + dateTimeDate.toGMTString());
                     break;
                 case 2:
-                    orderNumber = getData(i, length);
-                    System.out.println("orderNumber - " + orderNumber);
+                    orderNumberString = getData(i, length).replaceAll(" ", "");
+                    orderNumberInt = Integer.parseUnsignedInt(orderNumberString, 16);
+                    System.out.println("orderNumber - " + orderNumberInt);
                     break;
                 case 3:
-                    customerName = getData(i, length);
+                    customerName = getString(getData(i, length));
                     System.out.println("customerName - " + customerName);
                     break;
                 case 4:
@@ -90,9 +89,25 @@ public class Main {
 
     private static String getData(int indStart, int length) {
         StringBuilder data = new StringBuilder();
-        for (int i = length + indStart-1; i >= indStart; i--) {
+        for (int i = length + indStart - 1; i >= indStart; i--) {
             data.append(massString[i]).append(" ");
         }
         return data.toString();
+    }
+
+    private static String getString(String binaryStr) throws UnsupportedEncodingException {
+        String[] binaryMass = binaryStr.split(" ");
+        StringBuilder stringBuilder = new StringBuilder("");
+        for (String letter : binaryMass) {
+            stringBuilder.append((char) (byte) letter);
+            System.out.println(stringBuilder);
+//            stringBuilder.append( Integer.toBinaryString(Integer.parseInt(letter,16))).append(" ");
+        }
+        return stringBuilder.toString();
+
+//        binaryStr = binaryStr.replaceAll(" ", "");
+//        byte[] bytes = binaryStr.getBytes();
+//        binaryStr = new String(bytes, "UTF-16");
+//        return binaryStr
     }
 }
